cc90c9704d81e33b8b4299ba68e2d5176fc2bede
Caroline T. Ribeiro <caroltr.srs@gmail.com>
2017-09-10 11:18:30 -0300

    Correios OK

---
+++ DM106ProjetoFinal/DM106ProjetoFinal/CRMClient/CRMRestClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace DM106ProjetoFinal.CRMClient
+{
+    public class CRMRestClient
+    {
+        private HttpClient client;
+
+        public CRMRestClient()
+        {
+            client = new HttpClient();
+
+            // Endereço do CRM
+            client.BaseAddress = new Uri("http://siecolacrm.azurewebsites.net/api/");
+            
+            // Header para o JSON
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+                        
+            // Montando as credenciais em Base64
+            byte[] str1Byte = System.Text.Encoding.UTF8.GetBytes(string.Format("{0}:{1}", "crmwebapi", "crmwebapi"));
+            String plaintext = Convert.ToBase64String(str1Byte);
+            
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", plaintext);
+        }
+
+        public Customer GetCustomerByEmail(string email)
+        {
+            HttpResponseMessage response = client.GetAsync("customers/byemail?email=" + email).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse da resposta
+                Customer customer = (Customer)response.Content.ReadAsAsync<Customer>().Result;
+                return customer;
+            }
+            return null;
+        }
+    }
+}
--- DM106ProjetoFinal/DM106ProjetoFinal/Controllers/OrdersController.cs
+++ DM106ProjetoFinal/DM106ProjetoFinal/Controllers/OrdersController.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using DM106ProjetoFinal.Models;
-using System.Security.Principal;
 using System.Diagnostics;
+using DM106ProjetoFinal.CRMClient;
+using DM106ProjetoFinal.br.com.correios.ws;
 
 namespace DM106ProjetoFinal.Controllers
 {
@@ -18,8 +16,7 @@ namespace DM106ProjetoFinal.Controllers
     public class OrdersController : ApiController
     {
         private DM106ProjetoFinalContext db = new DM106ProjetoFinalContext();
-
-        // OK!!!!
+        
         // GET: api/Orders
         // Lista todos os pedidos
         [Authorize(Roles = "ADMIN")]
@@ -27,8 +24,7 @@ namespace DM106ProjetoFinal.Controllers
         {
             return db.Orders;
         }
-
-        // OK!!!!
+        
         // GET: api/Orders/5
         // Recuperar informação de um pedido através de seu id
         [ResponseType(typeof(Order))]
@@ -40,7 +36,7 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("O pedido não existe.");
             }
 
-            if (IsAuthorized(order))
+            if (IsAuthorized(order.Email))
             {
                 return Ok(order);
             }
@@ -49,8 +45,7 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("Acesso não autorizado.");
             }           
         }
-
-        // OK!!!
+        
         // GET: api/Orders/byEmail?email={email}
         // Lista todos os pedidos de um usuário através de seu email        
         [ResponseType(typeof(Order))]
@@ -74,43 +69,7 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("Acesso não autorizado.");
             }
         }
-
-        // PUT: api/Orders/5
-        /* [ResponseType(typeof(void))]
-        public IHttpActionResult PutOrder(int id, Order order)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != order.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        } */
-
-        // OK!!!
+        
         // POST: api/Orders
         [ResponseType(typeof(Order))]
         public IHttpActionResult PostOrder(Order order)
@@ -124,9 +83,16 @@ namespace DM106ProjetoFinal.Controllers
             order.Status = "novo";
             order.PesoTotal = 0;
             order.PrecoFrete = 0;
-            order.PrecoTotal = 0;
             order.DataPedido = DateTime.Now;
             order.DataEntrega = DateTime.Now;
+
+            // Calcular preço total
+            decimal precoTotal = 0;
+            foreach(OrderItem item in order.OrderItems)
+            {
+                precoTotal += item.Product.Preco * item.Quantidade;
+            }
+            order.PrecoTotal = precoTotal;
 
             // User.Identity.Name retorna o email do usuário autenticado
             order.Email = User.Identity.Name;            
@@ -137,10 +103,10 @@ namespace DM106ProjetoFinal.Controllers
             return CreatedAtRoute("DefaultApi", new { id = order.Id }, order);
         }
         
-        // GET: api/Orders/byEmail?email={email}
+        // PUT: api/Orders/byEmail?email={email}
         // Lista todos os pedidos de um usuário através de seu email        
         [ResponseType(typeof(Order))]
-        [HttpGet]
+        [HttpPut]
         [Route("api/Orders/Close")]
         public IHttpActionResult CloseOrder(int id)
         {
@@ -150,14 +116,16 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("O pedido não existe.");
             }
 
-            if (IsAuthorized(order))
+            if (IsAuthorized(order.Email))
             {
                 if(order.PrecoFrete != 0)
                 {
                     // Frete já foi calculado, portanto pedido pode ser fechado
                     order.Status = "fechado";
 
+                    db.Entry(order).State = EntityState.Modified;
                     db.SaveChanges();
+
                     return Ok(order);
                 }
                 else
@@ -170,8 +138,7 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("Acesso não autorizado.");
             }
         }
-
-        // OK!!!!
+        
         // DELETE: api/Orders/5
         [ResponseType(typeof(Order))]
         public IHttpActionResult DeleteOrder(int id)
@@ -182,7 +149,7 @@ namespace DM106ProjetoFinal.Controllers
                 return BadRequest("O pedido não existe.");
             }
 
-            if (IsAuthorized(order))
+            if (IsAuthorized(order.Email))
             {
                 db.Orders.Remove(order);
                 db.SaveChanges();
@@ -195,7 +162,130 @@ namespace DM106ProjetoFinal.Controllers
             }
         }
 
-        // TODO GetFreightRateAndDeliveryDate(int id)
+
+        // PUT: api/Orders/Freight?id={orderId}
+        // Calcula o frete de um pedido através de seu id
+        [ResponseType(typeof(Order))]
+        [HttpPut]
+        [Route("api/Orders/Freight")]
+        public IHttpActionResult GetFreightAndDate(int id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return BadRequest("O pedido não existe.");
+            }
+
+            if (IsAuthorized(order.Email))
+            {
+                Customer customer;
+
+                try
+                {
+                    CRMRestClient client = new CRMRestClient();
+                    customer = client.GetCustomerByEmail(User.Identity.Name);
+                }
+                catch(Exception e)
+                {
+                    return BadRequest("Falha ao consultar CEP: ocorreu um erro ao acessar o serviço de CRM.");
+                }
+
+                if(customer == null)
+                {
+                    return BadRequest("Falha ao consultar CEP: usuário não existente.");
+                }
+                else
+                {
+                    if(order.OrderItems.Count <= 0)
+                    {
+                        return BadRequest("O pedido não contêm itens.");
+                    }
+
+                    decimal alturaFinal = 0;
+                    decimal larguraFinal = 0;
+                    decimal comprimentoFinal = 0;
+                    decimal diametroFinal = 0;
+
+                    decimal pesoFinal = 0;
+                    
+                    foreach (OrderItem oi in order.OrderItems)
+                    {
+                        // Incrementando peso
+                        pesoFinal += oi.Product.Peso * oi.Quantidade;
+
+                        // Incrementando a largura
+                        larguraFinal += (oi.Product.Largura * oi.Quantidade);
+
+                        // Pegando o maior comprimento
+                        if(oi.Product.Comprimento > comprimentoFinal)
+                        {
+                            comprimentoFinal = oi.Product.Comprimento;
+                        }
+
+                        // Pegando a maior largura
+                        if (oi.Product.Altura > alturaFinal)
+                        {
+                            alturaFinal = oi.Product.Altura;
+                        }
+
+                        // Pegando o maior diametro
+                        if (oi.Product.Diametro > diametroFinal)
+                        {
+                            diametroFinal = oi.Product.Diametro;
+                        }
+                    }
+
+                    CalcPrecoPrazoWS correios = new CalcPrecoPrazoWS();
+
+                    string nCdServico = "40010"; // SEDEX
+                    string sCdCepOrigem = "69096010";
+                    string sCdCepDestino = customer.zip.Trim().Replace("-","");
+                    string nVIPeso = pesoFinal.ToString();
+                    int nCdFormato = 1; // Caixa
+                    decimal nVIComprimento = comprimentoFinal;
+                    decimal nVIAltura = alturaFinal;
+                    decimal nVILargura = larguraFinal;
+                    decimal nVIDiametro = diametroFinal;
+                    string sCdMaoPropria = "N";
+                    decimal nVIValorDeclarado = order.PrecoTotal;
+                    string sCdAvisoRecebimento = "S";
+
+                    cResultado corresiosResult;
+                    try
+                    {
+                        corresiosResult = correios.CalcPrecoPrazo("", "", nCdServico, sCdCepOrigem, sCdCepDestino, nVIPeso, nCdFormato,
+                            nVIComprimento, nVIAltura, nVILargura, nVIDiametro, sCdMaoPropria, nVIValorDeclarado, sCdAvisoRecebimento);
+                    }
+                    catch (Exception e)
+                    {
+                        return BadRequest("Falha ao calcular o frete e prazo de entrega: ocorreu um erro ao tentar acessar o serviço dos correios.");
+                    }
+
+                    if(!corresiosResult.Servicos[0].MsgErro.Equals(""))
+                    {
+                        return BadRequest("Falha ao calcular o frete e prazo de entrega: " + corresiosResult.Servicos[0].MsgErro);
+                    }
+
+                    int prazo = int.Parse(corresiosResult.Servicos[0].PrazoEntrega);
+                    decimal valorFrete = decimal.Parse(corresiosResult.Servicos[0].Valor);
+
+                    // Atualizar pedido
+                    order.PrecoFrete = valorFrete;
+                    order.DataEntrega = DateTime.Now.AddDays(prazo);
+                    order.PesoTotal = pesoFinal;
+                    order.PrecoTotal += valorFrete;
+                    
+                    db.Entry(order).State = EntityState.Modified;
+                    db.SaveChanges();
+                    
+                    return Ok(db.Orders.Find(id));
+                }
+            }
+            else
+            {
+                return BadRequest("Acesso não autorizado.");
+            }
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -210,16 +300,7 @@ namespace DM106ProjetoFinal.Controllers
         {
             return db.Orders.Count(e => e.Id == id) > 0;
         }
-
-        private bool IsAuthorized(Order order)
-        {
-            bool userOwnOrder = User.Identity.Name.Equals(order.Email);
-
-            Trace.TraceInformation(User.Identity.Name);
-
-            return (User.IsInRole("ADMIN") || userOwnOrder);
-        }
-
+        
         private bool IsAuthorized(string email)
         {
             bool userOwnOrder = User.Identity.Name.Equals(email);
